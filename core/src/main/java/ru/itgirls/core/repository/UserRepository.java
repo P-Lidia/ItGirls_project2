15294835c98fbc3780d7dfada9b1f1c4b6ae5706
marrgosha <marrgosha4@gmail.com>
2025-06-02 14:04:12 +0300

    fix: 02-06-2025 14:02 user repository fixed

--- core/src/main/java/ru/itgirls/core/repository/UserRepository.java
+++ core/src/main/java/ru/itgirls/core/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package ru.itgirls.core.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.itgirls.core.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
