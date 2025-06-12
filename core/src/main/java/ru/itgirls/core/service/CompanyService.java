8d1ef9785a95beae19469d107f0338e832fa30d0
Yulia <yuliayugaldina@gmai.com>
2025-06-12 23:42:26 +0500

    task-009_core_companyService create CompanyService and CompanyServiceImpl

--- core/src/main/java/ru/itgirls/core/service/CompanyService.java
+++ core/src/main/java/ru/itgirls/core/service/CompanyService.java
@@ -1,4 +1,21 @@
 package ru.itgirls.core.service;
 
+import ru.itgirls.core.dto.company.CompanyDto;
+import ru.itgirls.core.dto.company.CompanyCreateDto;
+import ru.itgirls.core.dto.company.CompanyUpdateDto;
+
+import java.util.List;
+
 public interface CompanyService {
+    CompanyDto createCompany(CompanyCreateDto companyCreateDto);
+
+    CompanyDto getCompanyById(Long id);
+
+    CompanyDto getCompanyByName(String name);
+
+    List<CompanyDto> getAllCompanies();
+
+    CompanyDto updateCompany(CompanyUpdateDto companyUpdateDto);
+
+    void deleteCompany(Long id);
 }
